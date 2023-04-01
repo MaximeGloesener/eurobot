16e8e5cbee67fedf27efc4c6568aaabf25ac5e3c
Maxime Gloesener <max.gleu@gmail.com>
2023-04-01 17:46:14 +0200

    init projet

---
+++ README.md
@@ -0,0 +1,5 @@
+## Projet Eurobot
+
+Objectif = aider l'équipe du robot pour la partie vision.
+
+Entraînement de Yolo sur les images annotées pour détecter les palets de différentes couleurs. 
---
+++ train.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+
+
+
+# Load a pretrained YOLO model (recommended for training)
+model = YOLO('yolov8n.pt')
+
+# Train the model using the 'coco128.yaml' dataset for 3 epochs
+results = model.train(data='dataset.yaml', epochs=200)
+
+# Evaluate the model's performance on the validation set
+results = model.val()
+
+# Perform object detection on an image using the model
+results = model('datasets/robot/images/20221116_210157.jpg')
+
+
+
