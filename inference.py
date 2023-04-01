d1b7b88f651064e089d0f488719afcfe02d83d3e
Maxime Gloesener <max.gleu@gmail.com>
2023-04-02 01:32:34 +0200

    add prediction

--- inference.py
+++ inference.py
@@ -9,5 +9,14 @@ model = YOLO('runs/detect/train/weights/best.pt')
 # récupérer le flux vidéo de la caméra (avec OpenCV) et faire la détection sur chaque image
 
 for img in os.listdir('datasets/robot/images'):
-    results = model('datasets/robot/images/'+img)
+    # save = True pour sauvegarder l'image avec les boites englobantes dans le dossier runs/detect/predict
+    results = model('datasets/robot/images/'+img, save=False)
+    for result in results:
+        print(result.boxes.xyxy)   # box with xyxy format, (N, 4)
+        print(result.boxes.xywh)   # box with xywh format, (N, 4)
+        print(result.boxes.xyxyn)  # box with xyxy format but normalized, (N, 4)
+        print(result.boxes.xywhn)  # box with xywh format but normalized, (N, 4)
+        print(result.boxes.conf)   # confidence score, (N, 1)
+        print(result.boxes.cls)    # cls, (N, 1)
+    break
 
